44f827417269eb4a3c733574376b088911d8fb75
Hugh Davidson <hugh.davidson@genero.com>
2021-10-19 22:38:19 +1100

    Complete FileIO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,42 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+printFile filePath chars = putStrLn ("============" ++ filePath) >> putStrLn chars
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles xs = void $ sequence (uncurry printFile <$> xs)
+-- void takes our IO (List ()) and throws away the inner contents
+-- In the standard library this is sequence_
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile fp = readFile fp >>= \chars -> return (fp, chars)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles = sequence . (getFile <$>)
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run fp = readFile fp >>= getFiles . lines  >>= printFiles
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = getArgs >>= void . sequence . (<$>) run
 
 ----
 
